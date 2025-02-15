5701beeeae5554be2f5e87385c2378aa93e1b910
Andrew Johnson <90214404+jesusfaithandwordisinmyheartalways@users.noreply.github.com>
2025-02-15 08:12:33 -0600

    Update Login.jsx

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
           }
 
           try {
-              const response = await fetch('http://localhost:3001/user/login', {
+              const response = await fetch('https://clothingstore-backend-ncru.onrender.com/user/login', {
                   method: 'POST',
                   headers: {"Content-Type": "application/json",},
                   body: JSON.stringify(loginData)
